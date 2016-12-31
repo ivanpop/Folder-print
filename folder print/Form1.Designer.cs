23df78f59afe8eba750a7be640818e875e430405
Ivan Popov <unfragable@data.bg>
2016-12-31 16:09:11 +0200

    31/12/16 - initial commit

---
+++ folder print/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace folder_print
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serviceButton = new System.Windows.Forms.Button();
+            this.folderBox = new System.Windows.Forms.TextBox();
+            this.folderBoxLabel = new System.Windows.Forms.Label();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // serviceButton
+            // 
+            this.serviceButton.Enabled = false;
+            this.serviceButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.serviceButton.Location = new System.Drawing.Point(12, 63);
+            this.serviceButton.Name = "serviceButton";
+            this.serviceButton.Size = new System.Drawing.Size(81, 57);
+            this.serviceButton.TabIndex = 0;
+            this.serviceButton.Text = "Start service";
+            this.serviceButton.UseVisualStyleBackColor = true;
+            this.serviceButton.Click += new System.EventHandler(this.serviceButton_Click);
+            // 
+            // folderBox
+            // 
+            this.folderBox.Location = new System.Drawing.Point(12, 37);
+            this.folderBox.Name = "folderBox";
+            this.folderBox.Size = new System.Drawing.Size(260, 20);
+            this.folderBox.TabIndex = 1;
+            // 
+            // folderBoxLabel
+            // 
+            this.folderBoxLabel.AutoSize = true;
+            this.folderBoxLabel.Location = new System.Drawing.Point(12, 13);
+            this.folderBoxLabel.Name = "folderBoxLabel";
+            this.folderBoxLabel.Size = new System.Drawing.Size(69, 13);
+            this.folderBoxLabel.TabIndex = 2;
+            this.folderBoxLabel.Text = "Select folder:";
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(197, 8);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 3;
+            this.browseButton.Text = "Browse...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.infoLabel.ForeColor = System.Drawing.Color.Black;
+            this.infoLabel.Location = new System.Drawing.Point(119, 84);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(85, 15);
+            this.infoLabel.TabIndex = 4;
+            this.infoLabel.Text = "Select a folder";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Double-click to show";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 143);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.folderBoxLabel);
+            this.Controls.Add(this.folderBox);
+            this.Controls.Add(this.serviceButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button serviceButton;
+        private System.Windows.Forms.TextBox folderBox;
+        private System.Windows.Forms.Label folderBoxLabel;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ folder print/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace folder_print
+{
+    public partial class Form1 : Form
+    {
+        bool serviceRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = MaximizeBox = false;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                this.Hide();
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void serviceButton_Click(object sender, EventArgs e)
+        {
+            if (serviceButton.Text == "Start service")
+            {                
+                serviceButton.Text = "Stop service";
+                infoLabel.Text = "Nothing to do";
+                serviceRunning = true;
+                runService();
+                this.WindowState = FormWindowState.Minimized;
+            }
+            else
+            {
+                serviceButton.Text = "Start service";
+                infoLabel.Text = "Start the service";
+                serviceRunning = false;
+            }
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    folderBox.Text = folderDialog.SelectedPath;
+                    serviceButton.Enabled = true;
+                    infoLabel.Text = "Start the service";
+                }
+            }
+        }
+
+        private async void runService()
+        {
+            do
+            {
+                if (Directory.GetFiles(folderBox.Text).Length > 0)
+                {
+                    foreach (string str in Directory.GetFiles(folderBox.Text))
+                    {
+                        ProcessStartInfo psi = new ProcessStartInfo(str);
+                        FileInfo file = new FileInfo(str);
+                        infoLabel.Text = "Processing " + str;
+                        psi.Verb = "print";
+                        Process.Start(psi);
+                        await Task.Delay(10000);
+                        while (IsFileLocked(file))
+                            Thread.Sleep(2000);
+                        File.Delete(str);
+                        if (serviceRunning == false)
+                            break;
+                    }
+                }
+                await Task.Delay(1000);
+            } while (serviceRunning == true);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        protected virtual bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            return false;
+        }
+    }
+}
